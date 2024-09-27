cadb1269af744f4d3f0d4c8f521c855d36f33e95
KiloByte328 <56058011+KiloByte328@users.noreply.github.com>
2024-09-27 19:54:48 +0700

    Initial for first lab

---
+++ lab1_1.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <mpi.h>
+
+int main (int argc, char *argv[])
+{
+    int n = 1;
+    int msg_tg;
+    MPI_Init(&argc, &argv);
+    int commsize, rank;
+    MPI_Comm_size(MPI_COMM_WORLD, &commsize);
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    int tags[16] = { 0 };
+    for (int i = 0; i <= 2;i++)
+    {
+        char msg[n];
+        for(int j = 0; j < n; j++)
+        {
+            msg[j] = rand() % 128;
+        }
+        msg_tg = rank;
+        MPI_Status stat;
+        double start_t = MPI_Wtime ();
+        for (;tags[rank] != 1;)
+        {
+        if (msg_tg == 15)
+        {
+            MPI_Sendrecv(&msg, n, MPI_CHAR, 0, msg_tg, &msg, n, MPI_CHAR, rank-1, MPI_ANY_TAG, MPI_COMM_WORLD, &stat);
+            tags[stat.MPI_TAG] = 1;
+            msg_tg = stat.MPI_TAG;
+        }
+            MPI_Sendrecv(&msg, n, MPI_CHAR, rank+1, msg_tg, &msg, n, MPI_CHAR, rank-1, MPI_ANY_TAG, MPI_COMM_WORLD, &stat);
+            tags[stat.MPI_TAG] = 1;
+            msg_tg = stat.MPI_TAG;
+        }
+        double end_t = MPI_Wtime();
+        printf ("n = %d , start time = %f , end time = %f , delta time = %f", n, start_t, end_t, start_t - end_t);
+        n = n * 1024;
+        for(int i = 0; i < 16; i++)
+        {
+            tags[i] = 0;
+        }
+    }
+}
